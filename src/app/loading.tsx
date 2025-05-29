24c1b510001fa4410eefac6e6e73115ee6d8e1e0
Mohammed Imran <mohammedimran86992@gmail.com>
2025-05-29 12:35:35 +0530

    Update loading component to adjust minimum height for improved layout consistency

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { LoadingSpinner } from "~/components/ui/loading-spinner";
 
 export default function Loading() {
 	return (
-		<div className="flex min-h-[calc(100vh-65px)] items-center justify-center">
+		<div className="flex min-h-[calc(100vh-155px)] items-center justify-center">
 			<LoadingSpinner className="h-12 w-12 animate-spin text-primary" />
 		</div>
 	);
