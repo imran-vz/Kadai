240622ffe2a00ac54974acecd6eb21b23fd796fb
Mohammed Imran <mohammedimran86992@gmail.com>
2025-05-31 20:26:02 +0530

    Add authentication check to items and orders pages
    - Integrated session authentication in both ItemsPage and OrdersPage components to ensure users are redirected to the login page if not authenticated.
    - This enhancement improves security by preventing unauthorized access to these pages.

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,15 @@
 import { HydrateClient, api } from "~/trpc/server";
 import ItemsPage from "./client";
+import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+	const session = await auth();
+
+	if (!session) {
+		redirect("/login");
+	}
+
 	void api.items.getAll.prefetch();
 
 	return (
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "~/server/auth";
 import { HydrateClient, api } from "~/trpc/server";
 import OrdersPage from "./client";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+	const session = await auth();
+
+	if (!session) {
+		redirect("/login");
+	}
+
 	void api.orders.getAll.prefetch({
 		limit: 10,
 		cursor: "0",
